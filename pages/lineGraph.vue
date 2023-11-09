929c77355a8ac53b471ee87e4d8d6e57fee07c3b
c21086065 <alshamsik@cardiff.ac.uk>
2023-11-09 15:18:51 +0000

    fetching the csv data from the backend (user story 9)

---
+++ pages/lineGraph.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        {{ data }}
+    </div>
+</template>
+
+<script setup>
+    const config = useRuntimeConfig();
+
+    const {data: data, error} = await useFetch(`${config.public.baseUrl}/api/line-graph`);
+    
+    console.log(data.value);
+    console.log(error.value);
+</script>
+
+<style scoped>
+
+</style>
