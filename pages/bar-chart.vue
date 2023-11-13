752f443f5c45ca9973a758a0d0a3e49ef41ea3f9
Jack Richards <richardsj31@cardiff.ac.uk>
2023-11-13 22:00:07 +0000

    Progress towards user story https://git.cardiff.ac.uk/c21052928/energy-data-centre-visualisation/-/issues/6
    Created a page that queries for relevant json data. This data is then displayed in the form of a clustered bar chart using D3.js.
    
    Comparing the average energy usage of different heating technologies to after energy efficiency improvements have been made.
    
    TODO. Add a legend.

--- pages/bar-chart.vue
+++ pages/bar-chart.vue
@@ -1,6 +1,7 @@
 <template>
     <div id="container" class="flex justify-center items-center h-screen">
         <h1 class="text-4xl font-bold text-center">Hello, world!</h1>
+        <!-- <svg id="bar-chart"></svg> -->
     </div>
 </template>
 
@@ -8,48 +9,119 @@
 import * as d3 from 'd3';
 
 try {
+    // Uses Nuxt 3s in-built useFetch method to fetch data from the API. This is done before page load, SSR.
     const json = await useFetch('http://127.0.0.1:8000/api/bar-chart');
-    console.log(json);
+    // Extract the data from the response.
+    let data = json.data.value;
     
     // Mounted
     onMounted(() => {
-        console.log("mounted");
-        // Declare the chart dimensions and margins.
-        const width = 640;
-        const height = 400;
-        const marginTop = 20;
-        const marginRight = 20;
-        const marginBottom = 30;
-        const marginLeft = 40;
+        // Extract unique heating technology types. This will be used for the x-axis.
+        // This work will probably be pushed to the backend in the future.
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
+        const heatingTypes = [...new Set([...data.thermal_characteristics_after_ee, ...data.thermal_characteristics_before_ee].map(d => d["Heating systems"]))];
 
-        // Declare the x (horizontal position) scale.
-        const x = d3.scaleUtc()
-            .domain([new Date("2023-01-01"), new Date("2024-01-01")])
-            .range([marginLeft, width - marginRight]);
+        const averagesBeforeEE = heatingTypes.map(heatingType => {
+            const values = data.thermal_characteristics_before_ee
+                // We filter out all the data not relating to the current heating type.
+                .filter(d => d["Heating systems"] === heatingType)
+                // We then return an array of the heat demand values for the current heating type
+                .map(d => d["Average annual heat demand kWh"]);
+            // These values are then passed to the d3.mean function, which returns the average.
+            // This average is then returned as an object with the heating type and average.
+            // The object is stored in the map created by the heatingTypes.map function.
+            return {
+                heatingType,
+                average: d3.mean(values),
+            };
+        });
 
-        // Declare the y (vertical position) scale.
-        const y = d3.scaleLinear()
-            .domain([0, 100])
-            .range([height - marginBottom, marginTop]);
+        // Same premise as above.
+        const averagesAfterEE = heatingTypes.map(heatingType => {
+            const values = data.thermal_characteristics_after_ee
+                .filter(d => d["Heating systems"] === heatingType)
+                .map(d => d["Average annual heat demand kWh"]);
 
-        // Create the SVG container.
-        const svg = d3.create("svg")
-            .attr("width", width)
-            .attr("height", height);
+            return {
+                heatingType,
+                average: d3.mean(values),
+            };
+        });
 
-        // Add the x-axis.
+        // Set up SVG container.
+        const svgWidth = 800;
+        const svgHeight = 400;
+        const margin = { top: 20, right: 20, bottom: 30, left: 40 };
+        const width = svgWidth - margin.left - margin.right;
+        const height = svgHeight - margin.top - margin.bottom;
+
+        // Get the maximum annual heat demand figures from both datasets.
+        const beforeMax = d3.max(data.thermal_characteristics_before_ee, d => d["Average annual heat demand kWh"]);
+        const afterMax = d3.max(data.thermal_characteristics_after_ee, d => d["Average annual heat demand kWh"]);
+
+        // Create SVG container.
+        const svg = d3.select("#container")
+            .append("svg")
+            .attr("width", svgWidth)
+            .attr("height", svgHeight)
+            .append("g")
+            // Move the SVG container to the top-left margin.
+            .attr("transform", `translate(${margin.left},${margin.top})`);
+
+        // Define scales. 
+        // A band scale is used for the x-axis, since we are dealing with categorical data. 
+        const xScale = d3.scaleBand()
+            .domain(heatingTypes)
+            .range([0, width])
+            .padding(0.1);
+
+        // Set the domain of the y-axis scale based on the maximum average value out of the two datasets.
+        // i.e., before and after.
+        // A linear scale is used because we are dealing with continuous data.
+        const yScale = d3.scaleLinear()
+            .range([height, 0])
+            .domain([0, d3.max([beforeMax, afterMax])]);
+
+        // Draw x-axis using the band scale.
         svg.append("g")
-            .attr("transform", `translate(0,${height - marginBottom})`)
-            .call(d3.axisBottom(x));
+            .attr("transform", `translate(0,${height})`)
+            .call(d3.axisBottom(xScale));
 
-        // Add the y-axis.
+        // Draw y-axis using the linear scale.
         svg.append("g")
-            .attr("transform", `translate(${marginLeft},0)`)
-            .call(d3.axisLeft(y));
+            .call(d3.axisLeft(yScale));
 
-        // Return the SVG element.
-        const container = d3.select("#container");
-        container.node().append(svg.node());
+        // Draw bars for thermal_characteristics_before_ee csv.
+        // Select all elements with class bar-before, and bind the data to it.
+        svg.selectAll(".bar-before")
+            .data(averagesBeforeEE)
+            // For each new data point, create a rect (rectangle) element.
+            .enter().append("rect")
+            .attr("class", "bar-before")
+            // Set the x-position of the bars based on the heating system type.
+            .attr("x", d => xScale(d.heatingType))
+            // Set the y-position of the bars based on the average annual heat demand for the heating technology.
+            .attr("y", d => yScale(d.average))
+            // Set the width of the bars to half of the bandwidth for clustering. Makes it easier to read.
+            .attr("width", xScale.bandwidth() / 2)
+            // Set the height of the bar using the average annual heat demand.
+            .attr("height", d => height - yScale(d.average))
+            // Style bars with red colour for 'before' energy efficiency improvements.
+            .style("fill", "red");
+
+        // Draw bars for thermal_characteristics_after_ee csv.
+        svg.selectAll(".bar-after")
+            .data(averagesAfterEE)
+            .enter().append("rect")
+            .attr("class", "bar-after")
+            .attr("x", d => xScale(d.heatingType) + xScale.bandwidth() / 2)
+            .attr("y", d => yScale(d.average))
+            .attr("width", xScale.bandwidth() / 2)
+            .attr("height", d => height - yScale(d.average))
+            // Style green for 'after' energy efficiency improvements.
+            .style("fill", "green"); 
+
+        // TODO. Add legend.
     });
 } catch (error) {
     console.log(error);
