4677bfb5993038fa74aaa88c5d5babb711f9446e
c21087406 <alsarmih@cardiff.ac.uk>
2023-11-14 15:40:15 +0000

    Myself and Jack have worked on the pie chart user story: https://git.cardiff.ac.uk/c21052928/energy-data-centre-visualisation/-/issues/7
    We have created an implementation that takes JSON data from an API request, and then renders a pie chart with d3.js using this data. This code uses and works with the proper formatting of the real CSVs we need to process.
    
    Co-authored-by: c21052928 <richardsj31@cardiff.ac.uk>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,9 @@
 export default defineNuxtConfig({
 	devtools: { enabled: true },
 	modules: ["@nuxt/ui"],
+	runtimeConfig:{
+		public: {
+			baseUrl: process.env.BASE_URL || "http://127.0.0.1:8000"
+		}
+	}
 });
