abdcbc69c7ceabd759d4209468f5e19888905717
Hamed Al Sarmi <alsarmih@cardiff.ac.uk>
2023-11-14 15:45:55 +0000

    Revert "Myself and Jack have worked on the pie chart user story:..."
    This reverts commit 4677bfb5993038fa74aaa88c5d5babb711f9446e

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,4 @@
 export default defineNuxtConfig({
 	devtools: { enabled: true },
 	modules: ["@nuxt/ui"],
-	runtimeConfig:{
-		public: {
-			baseUrl: process.env.BASE_URL || "http://127.0.0.1:8000"
-		}
-	}
 });
