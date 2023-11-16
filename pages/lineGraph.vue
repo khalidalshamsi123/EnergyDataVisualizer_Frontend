b9ded8485bf7b868f4f7fe78be1b54b0288bb796
c21086065 <alshamsik@cardiff.ac.uk>
2023-11-16 23:08:58 +0000

    Got the graph to display data from the CSV file, for the date and the Normalised_Resistance_heater_heat. This is progress towards user story -9

--- pages/lineGraph.vue
+++ pages/lineGraph.vue
@@ -15,13 +15,16 @@ const { data: data, error } = await useFetch(
 
 //testing, to fetch the values of 'normalised_resistance'
 //After Fetching the API data this code, iterates over the data array
-//and fetch the value of "Normalised_Resistance_heater_heat"
-data.value.forEach((item) => {
-  console.log(item["Normalised_Resistance_heater_heat"]);
+//and fetch the value of "Normalised_Resistance_heater_heat" and find the 'index'
+const resistanceHeaterHeatData = data.value.map((item) => {
+  return {
+    date: new Date(item.index),
+    value: item.Normalised_Resistance_heater_heat
+  };
 });
 
 //console logging the data
-console.log(data.value);
+console.log(resistanceHeaterHeatData);
 console.log(error.value);
 
 //creating the graph
@@ -41,13 +44,13 @@ onMounted(() => {
   // Declare the x (horizontal position) scale.
   const x = d3
     .scaleUtc()
-    .domain([new Date("2023-01-01"), new Date("2024-01-01")])
+    .domain(d3.extent(resistanceHeaterHeatData, (d) => d.date))
     .range([marginLeft, width - marginRight]);
 
   // Declare the y (vertical position) scale.
   const y = d3
     .scaleLinear()
-    .domain([0, 100])
+    .domain(d3.extent(resistanceHeaterHeatData, (d) => d.value))
     .range([height - marginBottom, marginTop]);
 
   // Create the SVG container.
@@ -69,30 +72,9 @@ onMounted(() => {
     .attr("transform", `translate(${marginLeft},0)`)
     .call(d3.axisLeft(y));
 
-  // Prepare the data
-  const data = [
-    { date: new Date("2023-01-01"), value: 10 },
-    { date: new Date("2023-02-01"), value: 20 },
-    { date: new Date("2023-03-01"), value: 30 },
-    { date: new Date("2023-04-01"), value: 40 },
-    { date: new Date("2023-05-01"), value: 50 },
-  ];
-
-  const data2 = [
-    { date: new Date("2023-01-01"), value: 10 },
-    { date: new Date("2023-02-01"), value: 30 },
-    { date: new Date("2023-03-01"), value: 30 },
-    { date: new Date("2023-04-01"), value: 45 },
-    { date: new Date("2023-05-01"), value: 50 },
-    { date: new Date("2023-06-01"), value: 60 },
-    { date: new Date("2023-07-01"), value: 70 },
-    { date: new Date("2023-08-01"), value: 80 },
-    { date: new Date("2023-09-01"), value: 90 },
-  ];
-
   // Update the data domain
-  x.domain(d3.extent(data, (d) => d.date));
-  y.domain(d3.extent(data, (d) => d.value));
+  x.domain(d3.extent(resistanceHeaterHeatData, (d) => d.date));
+  y.domain(d3.extent(resistanceHeaterHeatData, (d) => d.value));
 
   // Create the line generator
   const line = d3
@@ -100,21 +82,21 @@ onMounted(() => {
     .x((d) => x(d.date))
     .y((d) => y(d.value));
 
-  const line2 = d3
-    .line()
-    .x((d) => x(d.date))
-    .y((d) => y(d.value));
+  // const line2 = d3
+  //   .line()
+  //   .x((d) => x(d.date))
+  //   .y((d) => y(d.value));
 
   // Create the line path
-  const linePath = line(data);
-  const linePath2 = line2(data2);
+  const linePath = line(resistanceHeaterHeatData);
+  // const linePath2 = line2(data2);
 
   //making the area under the graph transparent
   svg.style("fill", "transparent");
 
   // Add the line path to the SVG element
-  svg.append("path").datum(data).attr("d", linePath).attr("stroke", "red");
-  svg.append("path").datum(data2).attr("d", linePath2).attr("stroke", "blue");
+  svg.append("path").datum(resistanceHeaterHeatData).attr("d", linePath).attr("stroke", "red");
+  // svg.append("path").datum(data2).attr("d", linePath2).attr("stroke", "blue");
 
   // Update the axes
   svg.select(".x-axis").call(d3.axisBottom(x));
