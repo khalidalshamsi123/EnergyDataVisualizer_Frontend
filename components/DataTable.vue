b6dc564713a4693ba7d7101dca0fc17b8d1e6658
Kavin Sundar <sundark@cardiff.ac.uk>
2023-11-16 21:05:59 +0000

    Implement table with fetching data from server
    - Incorporate DataTable component for reusablity
    - Allow fields to be chosen to be displayed
    
    Co-authored-by: c21077361 <vigneswaranj@cardiff.ac.uk>

---
+++ components/DataTable.vue
@@ -0,0 +1,18 @@
+<template>
+	<UTable :rows="tableData" />
+</template>
+
+<script setup>
+	const config = useRuntimeConfig();
+
+	const props = defineProps({
+		tableName: String,
+		fields: Array,
+	});
+
+	const { data: tableData } = await useFetch(`${config.public.baseURL}/tables/${props.tableName}`, {
+		query: {
+			fields: props.fields.join(","),
+		},
+	});
+</script>
--- pages/table.vue
+++ pages/table.vue
@@ -1,14 +1,6 @@
 <template>
-	<UTable :rows="users" />
+	<DataTable
+		:table-name="`ukerc_scotland_data`"
+		:fields="['LSOA11CD', 'Area (km2)', 'Road length (m)', 'Number of terraced resistance in 2018']"
+	/>
 </template>
-
-<script setup>
-	const users = [
-		{ id: 1, Location: "Cardiff", Temperature: "30", Date: "11/05/2022" },
-		{ id: 2, Location: "London", Temperature: "17", Date: "11/05/2022" },
-		{ id: 3, Location: "Manchester", Temperature: "21", Date: "11/05/2022" },
-		{ id: 4, Location: "Croydon", Temperature: "13", Date: "11/05/2022" },
-		{ id: 5, Location: "Llaneli", Temperature: "27", Date: "11/05/2022" },
-		{ id: 6, Location: "Llaneli", Temperature: "10", Date: "11/05/2022" },
-	];
-</script>
