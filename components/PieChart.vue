8a9469c54e92c8072f2971b467e84a4b2c8f276b
Frog <frog@localhost.localdomain>
2023-11-30 16:45:42 +0000

    Progress toward user story https://git.cardiff.ac.uk/c21052928/energy-data-centre-visualisation/-/issues/16
    Added page which renders data provided by API route in the form of a pie chart.
    
    Working towards adding %s to the end of each labels value.

--- components/PieChart.vue
+++ components/PieChart.vue
@@ -7,12 +7,14 @@
     </div>
   </template>
 <script setup>
-import { Chart as ChartJS, ArcElement, Title, Tooltip, Legend, Colors } from 'chart.js'
-import { Pie } from 'vue-chartjs'
+import { Chart as ChartJS, ArcElement, Title, Tooltip, Legend, Colors } from 'chart.js';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+import { Pie } from 'vue-chartjs';
 
 const props = defineProps({
     title: String,
-    data: Object
+    data: Object,
+    asPercentage: Boolean 
 });
 
 // Take title and JSON data passed into the component.
@@ -23,15 +25,32 @@ const jsonData = json.data.value;
 const data = [];
 const labels = [];
 
-for (const item of jsonData) {
-  data.push(item[1]);
-  // Capitalise first letter of label.
-  let label = item[0].charAt(0).toUpperCase() + item[0].slice(1);
-  labels.push(label);
+if(props.asPercentage) {
+  var sum = 0;
+
+  jsonData.forEach((item) => {
+    sum += item[1];
+  });
+
+  for (const item of jsonData) {
+    let percentage = (item[1] / sum) * 100;
+    percentage = Math.round((percentage + Number.EPSILON) * 100) / 100;
+    data.push(percentage);
+    // Capitalise first letter of label.
+    let label = item[0].charAt(0).toUpperCase() + item[0].slice(1);
+    labels.push(label);
+  }
+} else {
+  for (const item of jsonData) {
+    data.push(item[1]);
+    // Capitalise first letter of label.
+    let label = item[0].charAt(0).toUpperCase() + item[0].slice(1);
+    labels.push(label);
+  }
 }
 
 // Register
-ChartJS.register(ArcElement, Title, Tooltip, Legend, Colors);
+ChartJS.register(ArcElement, Title, Tooltip, Legend, Colors, ChartDataLabels);
 
 const chartData = ref({
   labels,
@@ -45,14 +64,25 @@ const chartOptions = ref({
   responsive: true,
   maintainAspectRatio: false,
   plugins: {
-
     colors: {
       enabled : true
     },
-
     legend: {
       enabled : true,
       position: 'bottom',
+    },
+    datalabels: {
+      formatter: function(value, context) {
+        return value + '%';
+      }
+    },
+    tooltip: {
+      callbacks: {
+        label: function(tooltipItem, data) {
+          let label = tooltipItem.yLabel + "%";
+          return label;
+        }
+      }
     },
     title: {
       display: true,
