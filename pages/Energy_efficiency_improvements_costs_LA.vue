602c9a392269e1656216186a5694e6e0464d5d56
Jack Richards <richardsj31@cardiff.ac.uk>
2023-12-03 21:53:03 +0000

    Progress towards https://git.cardiff.ac.uk/c21052928/energy-data-centre-visualisation/-/issues/16
    I have refactored the piecharts I made on Energy_Efficiency_improvment_costs_LA to use the new method of fetching and creating pie charts. Much simpler and cleaner.

--- pages/Energy_efficiency_improvements_costs_LA.vue
+++ pages/Energy_efficiency_improvements_costs_LA.vue
@@ -1,74 +1,59 @@
 <script setup>
 const config = useRuntimeConfig();
 
-// Fetch both average and sum of cost for energy efficiency improvements per dwelling type.
-var dwelling_type_json = await useFetch(`${config.public.baseUrl}/api/Energy_efficiency_improvements_costs_LA/dwelling_type`, {
+// Specific data we want to fetch from the API.
+// The 'filter' field is used to understand what column data we want to use in the calculations.
+const data_required = [
+  { "filter": "heating_type" },
+  { "filter": "dwelling_type" }
+];
+
+// Fetch the data from the API.
+var jsonData = await useFetch(`${config.public.baseUrl}/api/Energy_efficiency_improvements_costs_LA`, {
+  method: 'post',
+  body: JSON.stringify(data_required),
   transform: (response) => {
-    if(response.sum) {
-      // Convert each sum to be displayed in millions.
-      for (const item of response.sum) {
+    for (const key in response) {
+      for (const item of response[key]['sum']) {
+        // In this instance we know the sum is typically greater than £1 million.
+        // So we convert each sum to be in millions.
         item[1] = item[1] / 1000000;
       }
     }
+    // Array of objects containing the data for each chart.
     return response;
   }
 });
 
-dwelling_type_json = dwelling_type_json.data.value;
+jsonData = jsonData.data.value;
 
-// Percentage title.
-const average_cost_of_ee_per_dwelling_type_title = "Breakdown of average energy efficiency improvement costs per dwelling type (%)";
-// Figure title.
-const sum_cost_of_ee_per_dwelling_type_title = "Breakdown of overall energy efficiency improvement costs per dwelling type (£Million)";
+// Array of configuration objects for each chart. Each object contains the chart title, and whether the data should be displayed as a percentage.
+// The data each chart should use is added to each object in the loop below.
+const chartConfigurations = [
+  // Before.
+  { type: 'heating_type', title: 'Breakdown of average heat demand per heating technology before energy efficiency improvements (%)', asPercentage: true },
+  { type: 'heating_type', title: 'Breakdown of overall energy efficiency improvement costs per dwelling type (£Million)', asPercentage: false },
+  { type: 'dwelling_type', title: 'Breakdown of average energy efficiency improvement costs per heating type (%)', asPercentage: true },
+  { type: 'dwelling_type', title: 'Breakdown of overall energy efficiency improvement costs per heating type (£Million)', asPercentage: false },
+];
 
-// Fetch both average and sum of cost for energy efficiency improvements per heating type.
-var heating_type_json = await useFetch(`${config.public.baseUrl}/api/Energy_efficiency_improvements_costs_LA/heating_type`, {
-  transform: (response) => {
-    if(response.sum) {
-      // Convert each sum to be displayed in millions.
-      for (const item of response.sum) {
-        item[1] = item[1] / 1000000;
-      }
-    }
-    return response;
+// Loop over chartConfiguration and add data as a field to each object based on matching type.
+// Whether the sum or average is used for data depends on the asPercentage field.
+for (const config of chartConfigurations) {
+  // Check if the jsonData object has the required data for the current chart.
+  if(jsonData.hasOwnProperty(`${config.type}`)) {
+    var chart_data = jsonData[`${config.type}`];
+    config.data = chart_data[config.asPercentage ? 'average' : 'sum'];
   }
-});
-
-heating_type_json = heating_type_json.data.value;
-
-// Percentage title.
-const average_cost_of_ee_per_heating_type_title = "Breakdown of average energy efficiency improvement costs per heating type (%)";
-// Figure title.
-const sum_cost_of_ee_per_heating_type_title = "Breakdown of overall energy efficiency improvement costs per heating type (£Million)";
+}
 
 </script>
 <template>
-  <div>
+  <div v-for="config in chartConfigurations">
     <PieChart
-      :title=average_cost_of_ee_per_dwelling_type_title
-      :data=dwelling_type_json.average
-      :asPercentage=true
-    />
-  </div>
-  <div>
-    <PieChart
-      :title=sum_cost_of_ee_per_dwelling_type_title
-      :data=dwelling_type_json.sum
-      :asPercentage=false
-    />
-  </div>
-  <div>
-    <PieChart
-      :title=average_cost_of_ee_per_heating_type_title
-      :data=heating_type_json.average
-      :asPercentage=true
-    />
-  </div>
-  <div>
-    <PieChart
-      :title=sum_cost_of_ee_per_heating_type_title
-      :data=heating_type_json.sum
-      :asPercentage=false
+      :title=config.title
+      :data=config.data
+      :asPercentage=config.asPercentage
     />
   </div>
 </template>
