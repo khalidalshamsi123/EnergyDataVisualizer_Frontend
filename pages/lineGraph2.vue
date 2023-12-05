8afb3d018ce405e90400d7c6b41280fcf152ac67
c21086065 <alshamsik@cardiff.ac.uk>
2023-12-05 10:36:04 +0000

    Added Chart.js support and made new datasets for the graph. Progress towards user story -15

--- pages/lineGraph2.vue
+++ pages/lineGraph2.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <h1>Hello</h1>
-    <canvas id="myChart" style="width:100%;max-width:700px"></canvas>
+    <canvas id="myChart" style="width: 100%; max-width: 700px"></canvas>
   </div>
 </template>
 
 <script setup>
-import Chart from 'chart.js/auto'
+import Chart from "chart.js/auto";
 const config = useRuntimeConfig();
 
 //fetching the data from the API
@@ -25,10 +25,32 @@ const resistanceHeaterHeatData = data.value.map((item) => {
   };
 });
 
+//Similar to the code before just different data, same date property
+const ashpHeatData = data.value.map((item) => {
+  return {
+    date: new Date(item.index),
+    value: item.Normalised_ASHP_heat,
+  };
+});
+
+const gshpHeatData = data.value.map((item) => {
+  return {
+    date: new Date(item.index),
+    value: item.Normalised_GSHP_heat,
+  };
+});
+
+const gasBoilerHeatData = data.value.map((item) => {
+  return {
+    date: new Date(item.index),
+    value: item.Normalised_Gas_boiler_heat,
+  };
+});
 
 const xValues = resistanceHeaterHeatData.map((item) => item.date);
-const xMonthValues = xValues.map((date) => date.toLocaleString("en-US", { month: "short" }));
-
+const xMonthValues = xValues.map((date) =>
+  date.toLocaleString("en-US", { month: "short" })
+);
 
 const yValues = resistanceHeaterHeatData.map((item) => item.value);
 
@@ -36,24 +58,71 @@ new Chart("myChart", {
   type: "line",
   data: {
     labels: xMonthValues,
-    datasets: [{
-      fill: false,
-      lineTension: 0,
-      backgroundColor: "rgba(0,0,255,1.0)",
-      borderColor: "rgba(0,0,255,0.1)",
-      data: yValues
-    }]
+    datasets: [
+      {
+        fill: false,
+        lineTension: 0,
+        pointRadius: 0,
+        backgroundColor: "rgba(0,0,255,1.0)",
+        borderColor: "rgba(0,0,255,0.1)",
+        data: yValues,
+        label: "Resistance Heater heat"
+      },
+      {
+        fill: false,
+        lineTension: 0,
+        pointRadius: 0,
+        backgroundColor: "rgba(255,0,0,1.0)",
+        borderColor: "rgba(255,0,0,0.1)",
+        data: ashpHeatData.map((item) => item.value),
+        label: "ASHP Heat"
+      },
+      {
+        fill: false,
+        lineTension: 0,
+        pointRadius: 0,
+        backgroundColor: "rgba(0,128,0,1.0)",
+        borderColor: "rgba(0,128,0,0.1)",
+        data: gshpHeatData.map((item) => item.value),
+        label: "GSHP Heat"
+      },
+      {
+        fill: false,
+        lineTension: 0,
+        pointRadius: 0,
+        backgroundColor: "rgba(128,0,128,1.0)",
+        borderColor: "rgba(128,0,128,0.1)",
+        data: gasBoilerHeatData.map((item) => item.value),
+        label: "Gas Boiler Heat"
+      }
+      
+    ],
   },
   options: {
-    legend: {display: false},
+    legend: { display: true },
     scales: {
       xAxes: [{ type: "category" }],
-      yAxes: [{ticks: {min: 1, max:16}}],
-    }
-  }
+      yAxes: [{ ticks: { min: Math.min(...yValues), max: 16 } }],
+    },
+    plugins: {
+    tooltips: {
+      mode: "custom",
+      intersect: false,
+      position: "nearest",
+      callbacks: {
+        label: function (tooltipItem) {
+          const originalValue = tooltipItem.yLabel;
+          const formattedValue = originalValue.toFixed(8);
+          return `Original: ${originalValue}, Formatted: ${formattedValue}`;
+        },
+      },
+    },
+  },
+    animation: true,
+    spanGaps: true,
+    showLine: true,
+  },
 });
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
