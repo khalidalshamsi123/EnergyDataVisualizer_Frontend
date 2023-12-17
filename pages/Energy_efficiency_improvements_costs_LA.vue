d148b17c867c8418cffb9441c3044f2bf7df8a41
c21052928 <richardsj31@cardiff.ac.uk>
2023-12-17 18:28:28 +0000

    Due to time constraints we will not be making a generic component for the display of different chart types.
    But instead, because of the various edge-cases that exist, we will be creating the logic within the individual pages themselves.
    
    Progress towards https://git.cardiff.ac.uk/c21052928/energy-data-centre-visualisation/-/issues/21

--- pages/Energy_efficiency_improvements_costs_LA.vue
+++ pages/Energy_efficiency_improvements_costs_LA.vue
@@ -1,59 +1,286 @@
+<template>
+  <div class="page-container bg-gray-100 min-h-screen flex flex-col items-center justify-center">
+    <div class="graph-container rounded bg-white shadow-md mb-8">
+      <div v-for="option in selectedOptions">
+        <template v-if="option === 'Pie Chart'">
+          <PieChart v-if="pieChartData" :key="pieChartData.title" :title="pieChartData.title" :data="pieChartData.data"
+            :as-percentage="pieChartData.asPercentage" />
+        </template>
+        <template v-if="option === 'Bar Chart'">
+          <BarChart v-if="barChartData" :key="barChartData.title" :title="barChartData.title" :data="barChartData.data"
+            :max="barChartData.max" :legend="barChartData.legend" />
+        </template>
+      </div>
+    </div>
+
+    <div class="flex items-start">
+      <div class="filter-box bg-white rounded flex items-center w-[650px]">
+        <div class="relative">
+          <div class="length-checkit-options flex">
+            <p class="text-xl font-bold ml-2 mt-2">Chart Types</p>
+            <button class="text-xl ml-auto m-4 cursor-pointer mt-2" @click="toggleDropdown">
+              {{ chartTypesDowndownOpen ? "▲" : "▼" }}
+            </button>
+          </div>
+
+          <div class="flex p-1 pt-0 mr-auto" v-if="chartTypesDowndownOpen">
+            <div class="heat-cold-options bg-white rounded p-4 ml-2 h-[8.5rem]">
+              <div class="heat-container">
+                <input type="radio" id="heat" v-model="selectedHeatDwelling" value="heating_type" class="mr-2" />
+                <label for="heat" class="cursor-pointer">Heating Type</label>
+              </div>
+              <div class="heat-container">
+                <input type="radio" id="dwelling" v-model="selectedHeatDwelling" value="dwelling_type" class="mr-2" />
+                <label for="dwelling" class="cursor -pointer mt-2">Dwelling Type</label>
+              </div>
+            </div>
+
+            <div class="before-after-options bg-white rounded p-3 h-[8.5rem]">
+              <div class="option-container">
+                <input type="radio" id="average" v-model="selectedAverageSum" value="average" class="mr-2" />
+                <label for="average" class="cursor-pointer top-full">Average</label>
+              </div>
+              <div class="option-container">
+                <input type="radio" id="sum" v-model="selectedAverageSum" value="sum" class="mr-2" />
+                <label for="sum" class="cursor-pointer mt-2">Sum</label>
+              </div>
+            </div>
+
+            <div class="top-full w-96 bg-white rounded shadow p-4 h-[8.5rem] ml-2">
+              <div class="grid grid-cols-3 gap-4 justify-end">
+                <div v-for="option in ['Pie Chart', 'Bar Chart', 'Line Graph']" :key="option" class="option-container">
+                  <input type="checkbox" v-model="selectedOptions" :value="option" class="mr-2" />
+                  <p @click="selectOption(option)" class="cursor-pointer">{{ option }}</p>
+                </div>
+                <div v-for="option in ['Table', 'Geographical Map']" :key="option" class="option-container">
+                  <input type="checkbox" v-model="selectedOptions" :value="option" class="mr-2" />
+                  <p @click="selectOption(option)" class="cursor-pointer">{{ option }}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="filter-box bg-white rounded flex items-center ml-4 w-[210px]">
+        <div class="relative">
+          <div class="length-download-options flex">
+            <p class="text-xl font-bold ml-2 mt-2">Download</p>
+            <button class="text-xl mr-2 ml-auto m-4 cursor-pointer mt-2" @click="toggleDownloadDropdown">
+              {{ downloadDropbarOpen ? "▲" : "▼" }}
+            </button>
+          </div>
+
+          <div class="p-1 pt-0 mr-auto" v-if="downloadDropbarOpen">
+            <div class="download-snippet-options bg-white rounded p-3 h-[8.5rem]">
+              <div class="option-container">
+                <input type="radio" id="snippet" v-model="SelectDownloadSnippet" value="Snippet" class="mr-2" />
+                <label for="snippet" class="cursor-pointer top-full flex justify-center items-center">
+                  <span class="text-2xl">
+                    <font-awesome-icon icon="fa-solid fa-camera" />
+                  </span>
+                  <span class="text-center pl-2">Screenshot</span>
+                </label>
+              </div>
+              <div class="option-container">
+                <input type="radio" id="download" v-model="SelectDownloadSnippet" value="Download" class="mr-2" />
+                <label for="download" class="cursor-pointer top-full flex justify-center items-center">
+                  <span class="text-2xl pr-2">
+                    <font-awesome-icon icon="fa-solid fa-download" />
+                  </span>
+                  <span class="text-center">Download</span>
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
 <script setup>
 const config = useRuntimeConfig();
 
-// Specific data we want to fetch from the API.
-// The 'filter' field is used to understand what column data we want to use in the calculations.
-const data_required = [
-  { "filter": "heating_type" },
-  { "filter": "dwelling_type" }
-];
+const chartTypesDowndownOpen = ref(true);
+const downloadDropbarOpen = ref(true);
 
-// Fetch the data from the API.
-var jsonData = await useFetch(`${config.public.baseUrl}/api/Energy_efficiency_improvements_costs_LA`, {
-  method: 'post',
-  body: JSON.stringify(data_required),
-  transform: (response) => {
-    for (const key in response) {
-      for (const item of response[key]['sum']) {
-        // In this instance we know the sum is typically greater than £1 million.
-        // So we convert each sum to be in millions.
-        item[1] = item[1] / 1000000;
-      }
-    }
-    // Array of objects containing the data for each chart.
-    return response;
-  }
-});
+const selectedOptions = ref([]);
 
-jsonData = jsonData.data.value;
+const selectedHeatDwelling = ref(null);
+const selectedAverageSum = ref(null);
 
-// Array of configuration objects for each chart. Each object contains the chart title, and whether the data should be displayed as a percentage.
-// The data each chart should use is added to each object in the loop below.
-const chartConfigurations = [
-  // Before.
-  { type: 'heating_type', title: 'Breakdown of average heat demand per heating technology before energy efficiency improvements (%)', asPercentage: true },
-  { type: 'heating_type', title: 'Breakdown of overall energy efficiency improvement costs per dwelling type (£Million)', asPercentage: false },
-  { type: 'dwelling_type', title: 'Breakdown of average energy efficiency improvement costs per heating type (%)', asPercentage: true },
-  { type: 'dwelling_type', title: 'Breakdown of overall energy efficiency improvement costs per heating type (£Million)', asPercentage: false },
-];
+const SelectDownloadSnippet = ref(null);
 
-// Loop over chartConfiguration and add data as a field to each object based on matching type.
-// Whether the sum or average is used for data depends on the asPercentage field.
-for (const config of chartConfigurations) {
-  // Check if the jsonData object has the required data for the current chart.
-  if(jsonData.hasOwnProperty(`${config.type}`)) {
-    var chart_data = jsonData[`${config.type}`];
-    config.data = chart_data[config.asPercentage ? 'average' : 'sum'];
-  }
+const pieChartData = ref(null);
+const barChartData = ref(null);
+
+function toggleDropdown() {
+  chartTypesDowndownOpen.value = !chartTypesDowndownOpen.value;
 }
 
+function toggleDownloadDropdown() {
+  toggleDownloadDropbarOpen.value = !toggleDownloadDropbarOpen.value;
+}
+
+function selectOption(option) {
+  console.log(`Option '${option}' selected`);
+}
+
+watch([selectedHeatDwelling, selectedOptions, selectedAverageSum], async () => {
+  // If any of the needed options are left null, return.
+  if (!selectedHeatDwelling.value || !selectedOptions.value || !selectedAverageSum.value) {
+    return;
+  }
+
+  // Specific data we want to fetch from the API.
+  // The API selects the 'after' or 'before' data based on the 'rows' field.
+  // The 'filter' field is used to understand what column data we want to use in the calculations.
+  const data_required = [{ filter: selectedHeatDwelling.value }];
+
+  // Populate title and data variables.
+  const { data: jsonData } = await useFetch(`${config.public.baseUrl}/api/Energy_efficiency_improvements_costs_LA`, {
+    method: "post",
+    body: data_required,
+    transform: (response) => {
+      for (const key in response) {
+        for (const item of response[key]["sum"]) {
+          // Convert each sum to millions.
+          item[1] = item[1] / 1000000;
+        }
+      }
+      // Array of objects containing the data for each chart.
+      return response;
+    },
+  });
+
+  // Check if no data returned by API.
+  if (!jsonData || !jsonData.value) {
+    return;
+  }
+
+  var chartData = [];
+  var title = "";
+  // TODO should probably be toggleable in UI.
+  const asPercentage = false;
+
+  // Titles for graphs need to reflect which metric is being displayed.
+  if (selectedAverageSum.value === "average") {
+    chartData = jsonData.value[`${selectedHeatDwelling.value}`]["average"]
+    title = `Breakdown of average energy efficiency improvement costs per ${selectedHeatDwelling.value === "heating_type" ? "heating technology" : "dwelling type"} ${asPercentage ? "(%)" : "(£)"}`;
+  } else if (selectedAverageSum.value === "sum") {
+    chartData = jsonData.value[`${selectedHeatDwelling.value}`]["sum"]
+    title = `Breakdown of overall energy efficiency improvement costs per ${selectedHeatDwelling.value === "heating_type" ? "heating technology" : "dwelling type"} ${asPercentage ? "(%)" : "(£Million)"}`;
+  }
+
+  for (const option of selectedOptions.value) {
+    switch (option) {
+      case "Pie Chart":
+        // TODO: skip updates if selectedOptions was changed, but contains Pie Chart previously.
+        pieChartData.value = {
+          title: title,
+          data: chartData,
+          asPercentage,
+        };
+        break;
+      case "Bar Chart":
+        // Wasn't sure what to put for the legend.
+        barChartData.value = {
+          title: title,
+          data: { datasets: [chartData] },
+          max: Infinity,
+          legend: [selectedHeatDwelling.value === "heating_type" ? "Heating Technology" : "Dwelling Type"]
+        };
+        break;
+      case "Line Graph":
+        break;
+      case "Table":
+        break;
+      case "Geographical Map":
+        break;
+    }
+  }
+});
 </script>
-<template>
-  <div v-for="config in chartConfigurations">
-    <PieChart
-      :title=config.title
-      :data=config.data
-      :asPercentage=config.asPercentage
-    />
-  </div>
-</template>
+
+<style scoped>
+.combined-container {
+  display: flex;
+  text-align: center;
+  align-items: center;
+}
+
+.option-container {
+  display: flex;
+  align-items: center;
+  @apply pb-1;
+}
+
+.thistext {
+  margin-right: -65px;
+  align-items: left;
+}
+
+.heat-container {
+  display: flex;
+  align-items: center;
+}
+
+.icon {
+  margin-left: 10px;
+  margin-top: px;
+}
+
+.anothericon {
+  margin-left: 1px;
+  margin-top: px;
+}
+
+.another-cotainer {
+  position: relative;
+  top: 1200px;
+}
+
+.download-snippet-options,
+.before-after-options,
+.heat-cold-options {
+  top: 100%;
+  left: 0;
+  padding: 0.75rem;
+  border-radius: 0.375rem;
+  background-color: white;
+  box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+}
+
+.before-after-options input[type="radio"],
+.download-snippet-options input[type="radio"],
+.heat-cold-options input[type="radio"] {
+  display: none;
+}
+
+.download-snippet-options label,
+.before-after-options label,
+.heat-cold-options label {
+  cursor: pointer;
+  padding: 0.75rem;
+  width: 100%;
+  border: 1px solid #e2e8f0;
+  border-radius: 0.25rem;
+}
+
+.download-snippet-options input[type="radio"]:checked+label,
+.before-after-options input[type="radio"]:checked+label,
+.heat-cold-options input[type="radio"]:checked+label {
+  background-color: #ced4d8;
+  color: white;
+}
+
+.length-checkit-options {
+  width: 660px;
+}
+
+.length-download-options {
+  height: 52px;
+  width: 210px;
+}
+</style>
