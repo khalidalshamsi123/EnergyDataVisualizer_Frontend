637ef59bdac9049f741aaa6af74db5d92316c4d2
Kavin Sundar <sundark@cardiff.ac.uk>
2023-12-18 00:48:21 +0000

    Fix fontawesome ssr mismatch warning.
    See https://github.com/FortAwesome/vue-fontawesome/issues/394 for the details of the workaround

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	devtools: { enabled: true },
-	modules: ["@nuxt/ui"],
-	runtimeConfig: {
-		public: {
-			baseUrl: process.env.BASE_URL || "http://127.0.0.1:8000",
-		},
-	},
-	css: [
-		'@fortawesome/fontawesome-svg-core/styles.css'
-	]
+    devtools: { enabled: true },
+    modules: ["@nuxt/ui"],
+    runtimeConfig: {
+        public: {
+            baseUrl: process.env.BASE_URL || "http://127.0.0.1:8000",
+        },
+    },
+    css: ["@fortawesome/fontawesome-svg-core/styles.css"],
+    build: {
+        transpile: [
+            "@fortawesome/fontawesome-svg-core",
+            "@fortawesome/free-solid-svg-icons",
+            "@fortawesome/vue-fontawesome",
+        ],
+    },
 });
